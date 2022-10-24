62f87c7b203bdd8066ac093533885ca2be76cc76
Jeremy Grecu <jeremy.grecu@sap.com>
2022-10-24 13:38:25 +0100

    refactor

--- src/test/java/io/jeremy/account/controllers/EmployeeControllerTest.java
+++ src/test/java/io/jeremy/account/controllers/EmployeeControllerTest.java
@@ -1,50 +1,65 @@
 package io.jeremy.account.controllers;
 
-import io.jeremy.account.respository.PaymentRepository;
-import io.jeremy.account.respository.UserRepository;
-import io.jeremy.account.security.RestAuthenticationEntryPoint;
-import io.jeremy.account.service.JpaUserDetailsService;
-import io.jeremy.account.service.LoggingService;
 import io.jeremy.account.service.PaymentService;
+import io.jeremy.account.web.responses.UserPaymentsResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(EmployeeController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EmployeeControllerTest {
 
-
-
     @Autowired
+    private WebApplicationContext context;
+
+    @MockBean
+    private PaymentService paymentService;
+
     private MockMvc mockMvc;
 
-    @MockBean
-    PaymentService paymentService;
-
-    @MockBean
-    PaymentRepository paymentRepository;
-
-    @MockBean
-    UserRepository userRepository;
-
-    @MockBean
-    JpaUserDetailsService jpaUserDetailsService;
-
-    @MockBean
-    RestAuthenticationEntryPoint restAuthenticationEntryPoint;
-
-    @MockBean
-    LoggingService loggingService;
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
     @Test
-    @WithMockUser(username = "jeremy", authorities = "ROLE_USER")
+    @WithMockUser("john@acme.com")
+    void whenGivenTheRightUser() throws Exception {
+        when(paymentService.getUserPayments("john@acme.com"))
+                .thenReturn(List.of(new UserPaymentsResponse("John", "Doe",
+                        "January-2022", "1")));
+
+        mockMvc.perform(get("/api/empl/payment").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMINISTRATOR")
     void name() throws Exception {
-        mockMvc.perform(get("/api/empl/payment")).andExpect(status().isOk());
+        mockMvc.perform(get("/api/empl/payment").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void name_unauthenticated() throws Exception {
+        mockMvc.perform(get("/api/empl/payment").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
     }
 }
