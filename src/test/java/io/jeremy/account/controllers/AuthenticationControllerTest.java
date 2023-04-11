8b216b9697fbd7508e8c8a70b2865b276f281750
Jeremy Grecu <jgrecu@gmail.com>
2023-04-11 13:21:40 +0100

    created Test for the AuthenticationController

--- src/test/java/io/jeremy/account/controllers/AuthenticationControllerTest.java
+++ src/test/java/io/jeremy/account/controllers/AuthenticationControllerTest.java
@@ -1,32 +1,25 @@
 package io.jeremy.account.controllers;
 
-import io.jeremy.account.service.JpaUserDetailsService;
-import io.jeremy.account.service.LoggingService;
-import io.jeremy.account.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jeremy.account.dto.requests.ChangePasswordRequest;
 import io.jeremy.account.dto.requests.UserRequest;
 import io.jeremy.account.dto.responses.ChangePassResponse;
 import io.jeremy.account.dto.responses.UserResponse;
+import io.jeremy.account.service.JpaUserDetailsService;
+import io.jeremy.account.service.LoggingService;
+import io.jeremy.account.service.UserService;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -38,6 +31,9 @@ class AuthenticationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @MockBean
     private UserService userService;
 
@@ -46,32 +42,20 @@ class AuthenticationControllerTest {
 
     @MockBean
     private LoggingService loggingService;
-
-//    @BeforeEach
-//    public void setup() {
-//        mockMvc = MockMvcBuilders
-//                .webAppContextSetup(this.webApplicationContext)
-//                .apply(springSecurity())
-//                .build();
-//    }
-
+    
     @Test
     void shouldCreateANewUSer() throws Exception {
-        String userRequest = """
-                {
-                    "name": "John",
-                    "lastname": "Doe",
-                    "email": "johndoe@acme.com",
-                    "password": "bZPGqH7fTJWW"
-                }""";
+        UserRequest request = new UserRequest("John", "Doe",
+                "johndoe@acme.com", "bZPGqH7fTJWW");
 
         UserResponse userResponse = new UserResponse(1L, "John", "Doe",
                 "johndoe@acme.com", List.of("ROLE_USER"));
+
         when(userService.addUser(any(UserRequest.class))).thenReturn(userResponse);
 
         mockMvc.perform(post("/api/auth/signup")
                         .accept(MediaType.APPLICATION_JSON)
-                        .content(userRequest)
+                        .content(objectMapper.writeValueAsString(request))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", Matchers.is(1)))
@@ -80,22 +64,28 @@ class AuthenticationControllerTest {
                 .andExpect(jsonPath("$.email").value("johndoe@acme.com"))
                 .andExpect(jsonPath("$.roles[0]").value("ROLE_USER"))
                 .andDo(print());
+
+        verify(userService, times(1)).addUser(any(UserRequest.class));
     }
 
     @Test
     @WithMockUser(username = "johndoe@acme.com")
     void shouldChangePasswordOfExistingUser() throws Exception {
+        ChangePasswordRequest changePasswordRequest = new ChangePasswordRequest("bZPGqH7fTJW12");
         ChangePassResponse passResponse = new ChangePassResponse("johndoe@acme.com",
                 "The password has been updated successfully");
+
         when(userService.updatePassword(anyString(), anyString())).thenReturn(passResponse);
 
         mockMvc.perform(post("/api/auth/changepass")
                 .accept(MediaType.APPLICATION_JSON)
-                .content("{\"new_password\": \"bZPGqH7fTJW12\"}")
+                .content(objectMapper.writeValueAsString(changePasswordRequest))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.email").value("johndoe@acme.com"))
                 .andExpect(jsonPath("$.status").isNotEmpty())
                 .andDo(print());
+
+        verify(userService, times(1)).updatePassword(anyString(), anyString());
     }
 }
